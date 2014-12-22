3fda94c992d58c230affa240458fa7623c2fde50
trox <trox@aquitaine>
2014-12-22 16:37:23 -0600

    Pass environment via .env file

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 import os
 
-DEBUG = True
-
+# Defaults, overridden by os.environ()
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 THREADS_PER_PAGE = 2
-
 CSRF_ENABLED = True
 
 PORT = 3002
+
--- flaskapp.py
+++ flaskapp.py
@@ -2,4 +2,4 @@ from app import app
 import os
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', app.config['PORT'])), debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', app.config['PORT'])), debug=os.getenv("DEBUG"))

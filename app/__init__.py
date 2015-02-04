050c1fc404085c67da67a7aa3d9c8a56110c14eb
trox <trox@aquitaine>
2015-02-04 13:04:46 -0600

    implement SITE_HOSTNAME

--- app/__init__.py
+++ app/__init__.py
@@ -12,19 +12,23 @@ redis = Redis(host='redis', port=6379)
 app.config.from_object('config')
 app.config.from_envvar('FLASKAPP_SETTINGS', silent=True)
 
+
 @app.route('/')
 def show_index():
     hostname = socket.gethostname()
+    site-hostname = os.environ['SITE_HOSTNAME']
     return render_template('home.html', **locals())
 
 @app.route('/about')
 def show_about():
     hostname = socket.gethostname()
+    site-hostname = os.environ['SITE_HOSTNAME']
     return render_template('about.html', **locals())
 
 @app.route('/contact')
 def show_contact():
     hostname = socket.gethostname()
+    site-hostname = os.environ['SITE_HOSTNAME']
     return render_template('contact.html', **locals())
 
 @app.route('/redistest')
